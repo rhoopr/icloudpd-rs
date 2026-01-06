1c9a17f37268534e3bc9a3981e9cd15970df7d97
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2026-01-05 21:58:40 -0800

    Add maintainer notice to README
    Added a notice for seeking a maintainer for the project.

---
+++ src/pyicloud_ipd/raw_policy.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class RawTreatmentPolicy(Enum):
+    AS_IS = "as-is"
+    AS_ORIGINAL = "as-original"
+    AS_ALTERNATIVE = "as-alternative"
+
+    def __str__(self) -> str:
+        return self.name
